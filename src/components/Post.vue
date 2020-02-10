5cb440dfa02592fd65cc748103f3bbf339004bd9
HotPotatoC <43059506+HotPotatoC@users.noreply.github.com>
2020-02-10 18:37:39 +0700

    Added tooltip on bookmark hover

--- src/components/Post.vue
+++ src/components/Post.vue
@@ -1,5 +1,5 @@
 <template>
-  <div :class="grid" class="w-full md:w-1/2 p-6 m-2 bg-white shadow-md rounded-lg">
+  <div :class="grid" class="w-full md:w-1/2 p-6 m-2 mb-8 bg-white shadow-md rounded-lg">
     <h1 class="text-4xl font-bold">
       <router-link :to="'/post/' + article.id">{{ article.title }}</router-link>
     </h1>
@@ -11,41 +11,57 @@
     <div class="flex justify-between items-baseline pt-12">
       <span class>
         <p class="text-gray-600">By {{ article.user }}</p>
-        <p class="text-gray-600">{{ article.date_posted }}</p>
       </span>
-      <button @click="isBookmarked = !isBookmarked" class="bookmark-btn p-2 rounded-full hover:bg-teal-100 focus:outline-none">
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          width="24"
-          height="24"
-          viewBox="0 0 24 24"
-          :fill="isBookmarked ? '#4FD1C5' : 'none'"
-          stroke="#4FD1C5"
-          stroke-width="2"
-          stroke-linecap="round"
-          stroke-linejoin="round"
-          class="feather feather-bookmark"
+      <div class="block text-right">
+        <Tooltip
+          :msg="isBookmarked ? 'Added to bookmark!': 'Add to bookmark'"
+          v-show="showTooltip"
+        />
+        <button
+          @click="isBookmarked = !isBookmarked"
+          @mouseover="showTooltip = !showTooltip"
+          @mouseout="showTooltip = false"
+          class="bookmark-btn p-2 rounded-full hover:bg-teal-100 focus:outline-none"
         >
-          <path d="M19 21l-7-5-7 5V5a2 2 0 0 1 2-2h10a2 2 0 0 1 2 2z" />
-        </svg>
-      </button>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="24"
+            height="24"
+            viewBox="0 0 24 24"
+            :fill="isBookmarked ? '#4FD1C5' : 'none'"
+            stroke="#4FD1C5"
+            stroke-width="2"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            class="feather feather-bookmark"
+          >
+            <path d="M19 21l-7-5-7 5V5a2 2 0 0 1 2-2h10a2 2 0 0 1 2 2z" />
+          </svg>
+        </button>
+      </div>
     </div>
   </div>
 </template>
 
 <script>
+import Tooltip from "./Tooltip";
+
 export default {
   props: ["grid", "article"],
+  components: {
+    Tooltip
+  },
   data() {
     return {
-      isBookmarked: false
-    }
+      isBookmarked: false,
+      showTooltip: false
+    };
   }
 };
 </script>
 
 <style lang="scss" scoped>
 .bookmark-btn {
-  transition: 0.2s;
+  transition: 0.35s;
 }
 </style>
